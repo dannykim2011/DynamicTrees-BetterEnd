a101d01b1a43e4384d91fe23eca84af71fbf1239
dannykim2011 <danny4257@naver.com>
2026-07-16 14:44:30 +0900

    update

--- src/main/java/com/dannykim/dtbetterend/DynamicTreesBetterEnd.java
+++ src/main/java/com/dannykim/dtbetterend/DynamicTreesBetterEnd.java
@@ -2,6 +2,8 @@ package com.dannykim.dtbetterend;
 
 import com.ferreusveritas.dynamictrees.api.registry.RegistryHandler;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -12,6 +14,7 @@ public final class DynamicTreesBetterEnd {
 
     public DynamicTreesBetterEnd() {
         RegistryHandler.setup(MOD_ID);
+        DistExecutor.safeRunWhenOn(Dist.CLIENT, () -> com.dannykim.dtbetterend.client.ThickBranchRingsSource::register);
         if (ModList.get().isLoaded("dynamictreesplus")) {
             FMLJavaModLoadingContext.get().getModEventBus().register(
                     com.dannykim.dtbetterend.systems.mushroom.DTPlusRegistries.class
